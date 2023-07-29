af41d50e75d759d8c6ecd94a8d27e2bdf3931893
toshishimi <sc.104.k@gmail.com>
2023-07-29 16:37:44 +0900

    その他の機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, except: [:index, :create, :show, :update]
   before_action :move_to_index, only: :edit
 
   def index
